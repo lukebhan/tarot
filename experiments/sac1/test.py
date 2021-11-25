5567556995ef039aa185ec5aaa19e03f6760d8a9
Luke Bhan <luke.bhan@vanderbilt.edu>
2021-11-24 22:58:17 -0600

    fix

--- experiments/sac1/test.py
+++ experiments/sac1/test.py
@@ -1,17 +1,67 @@
 import gym
 import gym_tarot
 import numpy as np
+from stable_baselines3 import PPO
 from stable_baselines3 import SAC
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
 import os
+
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level.
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print(f"Saving new best model to {self.save_path}")
+                  self.model.save(self.save_path)
+
+        return True
 
 log_dir = "tmp/"
 os.makedirs(log_dir, exist_ok=True)
 
 env=gym.make('Tarot-v0')
 env=Monitor(env, log_dir)
+callback = SaveOnBestTrainingRewardCallback(check_freq=100000, log_dir=log_dir)
 
-policy_kwargs = dict(net_arch=dict(pi=[256, 128, 128], qf=[256, 128, 128]))
+#policy_kwargs = dict(net_arch=[256, 256])
+policy_kwargs = dict(net_arch=dict(pi=[128, 128], qf=[128, 128]))
 model = SAC("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
-model.learn(5000000)
+model.learn(5000000, callback=callback)
 model.save('model_no_fault')
